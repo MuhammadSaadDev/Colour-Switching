e77135d5b0f71a474826fe784c1aae1cac544162
MuhammadSaadDev <111735201+MuhammadSaadDev@users.noreply.github.com>
2022-10-06 02:05:39 +0500

    First push
    First push color switching

---
+++ color_switching.js
@@ -0,0 +1,19 @@
+
+// let button1 = document.querySelector("#button1");
+// let button2 = document.querySelector("#button2");
+function change1() {
+    document.getElementById("button1").style.backgroundColor="orangered";
+    document.getElementById("button2").style.backgroundColor="purple";
+    }
+function original1() {
+    document.getElementById("button1").style.backgroundColor="purple";
+    document.getElementById("button2").style.backgroundColor="orangered";
+  }
+function change2() {
+    document.getElementById("button1").style.backgroundColor="orangered";
+    document.getElementById("button2").style.backgroundColor="purple";
+    }
+function original2() {
+    document.getElementById("button1").style.backgroundColor="purple";
+    document.getElementById("button2").style.backgroundColor="orangered";
+  }
